9f0ca0b7bf290592a6f03f80b7e46296c4975944
Joaquin Noe <joako.n96@gmail.com>
2020-07-06 01:24:58 -0300

    Create detail component which should be renamed to inventario-component

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  components: any[];
+  constructor() {
+    const c = [];
+    for (let i = 0; i < 50; i++) {
+      c[i] = {
+        Name: `Component-${i}`,
+        Price: `${Math.random() * (15000 - 500) + 500}`,
+        Quantity: `${Math.random() * (100 - 10) + 10}`
+      };
+    }
+    this.components = c;
   }
 
+  ngOnInit(): void { }
 }
